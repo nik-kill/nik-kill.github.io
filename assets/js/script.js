385ec9711b7de7dda58b2904ad92f7fe0baa583f
Nikhil Gupta <nikhil062000@gmail.com>
2021-02-01 04:46:49 +0530

    onload funtion call, parameter dependent

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,17 +10,18 @@ function menuAnimate(x) {
 }
 
 var i = 0;
-var txt = "Hey there !!! :)";
+// var txt = "Hey there !!! :)";
 var speed = 100; /* The speed/duration of the effect in milliseconds */
-function typeWriter() {
+function typeWriter(txt) {
     if (i < txt.length) {
         document.getElementById("welcome_text").innerHTML += txt.charAt(i);
         i++;
-        setTimeout(typeWriter, speed);
+        setTimeout(typeWriter(txt), speed);
     }
 }
 
 function displayH1(pos){
+    
     document.getElementById("welcome_text").innerHTML = pos;
 }
 
